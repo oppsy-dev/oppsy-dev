a935e5ae2f5bf9ebf1b5125e92b64e5b4349f740
Mr-Leshiy <leshiy12345678@gmail.com>
2026-04-30 15:15:08 +0200

    wip

--- backend/notifier/src/email/mod.rs
+++ backend/notifier/src/email/mod.rs
@@ -1,5 +1,7 @@
 use lettre::{
-    Address, AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor, message::Mailbox,
+    Address, AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor,
+    message::{Mailbox, header::ContentType},
+    transport::smtp::authentication::Credentials,
 };
 use reqwest::Url;
 
@@ -11,7 +13,20 @@ pub struct EmailNotifier {
 
 impl EmailNotifier {
     pub async fn new(smtp_url: Url) -> anyhow::Result<Self> {
-        let smtp = AsyncSmtpTransport::<Tokio1Executor>::from_url(smtp_url.as_str())?.build();
+        let host = smtp_url
+            .host_str()
+            .ok_or_else(|| anyhow::anyhow!("Missing SMTP host"))?;
+        let port = smtp_url
+            .port()
+            .ok_or_else(|| anyhow::anyhow!("Missing SMTP port"))?;
+        let creds = Credentials::new(
+            smtp_url.username().to_string(),
+            smtp_url.password().unwrap_or_default().to_string(),
+        );
+        let smtp = AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(host)?
+            .port(port)
+            .credentials(creds)
+            .build();
         anyhow::ensure!(
             smtp.test_connection().await?,
             "Cannot connect to SMTP server {smtp_url}"
@@ -34,7 +49,10 @@ impl Notifier for EmailNotifier {
         conf: Self::EventConf,
         payload: impl serde::Serialize + Send + Sync,
     ) -> anyhow::Result<()> {
-        let mut email_builder = Message::builder().from(Mailbox::new(None, conf.from));
+        let mut email_builder = Message::builder()
+            .from(Mailbox::new(None, conf.from))
+            .subject("OSV Vulnerability Report")
+            .header(ContentType::TEXT_PLAIN);
 
         email_builder = conf.to.into_iter().fold(email_builder, |b, to_addr| {
             b.to(Mailbox::new(None, to_addr))
