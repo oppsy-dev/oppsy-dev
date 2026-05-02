68d7626214124d16803e931f52236a1aa370a97b
oppsy-dev <leshiy12345678@gmail.com>
2026-05-02 17:23:27 +0200

    fix

--- frontend/src/pages/ChannelsDashboardPage/Channels/CreateChannelModal/EmailChannelForm/EmailChannelForm.tsx
+++ frontend/src/pages/ChannelsDashboardPage/Channels/CreateChannelModal/EmailChannelForm/EmailChannelForm.tsx
@@ -8,13 +8,13 @@ import styles from '../CreateChannelModal.module.css';
 export const EMAIL_TEMPLATE_SCHEMA = `subject: string\nbody: string`;
 
 export const EMAIL_DEFAULT_TEMPLATE = `subject: """
-[OPPSY] New vulnerabilities detected in \(_workspace_name) \(_manifest_name)
+[OPPSY] New vulnerabilities detected in \\(_workspace_name) \\(_manifest_name)
 """
 
 body: """
 OPPSY detected
-Workspace:      \(_workspace_name)
-Manifest:       \(_manifest_name) (\(_manifest_type))
+Workspace:      \\(_workspace_name)
+Manifest:       \\(_manifest_name) (\\(_manifest_type))
 Review each finding at https://osv.dev and assess whether your project is affected.
 --
 To stop receiving these emails, disable or delete this notification channel in OPPSY.
