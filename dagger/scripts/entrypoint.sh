a312dd22f7c89e2ee959c3e13b297c8bc3bfff95
oppsy-dev <leshiy12345678@gmail.com>
2026-05-05 16:20:47 +0200

    wip

--- dagger/scripts/entrypoint.sh
+++ dagger/scripts/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /core-db
-atlas migrate apply --url sqlite:///data/oppsy.db --env sqlite --config file:///core-db/atlas.hcl
+atlas migrate apply --url sqlite:///data/core-db/oppsy.db --env sqlite --config file:///core-db/atlas.hcl
 exec /usr/local/bin/service
